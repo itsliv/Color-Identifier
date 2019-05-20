ab946c85294ba7a56b3bfac6d8ceb44ef7f529c4
Liv Reinis <ljreinis@gmail.com>
2019-05-20 10:47:27 -0700

    Start of Project

---
+++ Color Identifier/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Color Identifier
+//
+//  Created by Liv J Reinis on 5/13/19.
+//  Copyright © 2019 Liv J Reinis. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
